68f9c67a46a2c05f42974ba49558ad750d0511d8
Mark Isom <markandrewisom@gmail.com>
2023-03-21 08:57:06 -0400

    Test

--- bikini/theBook.sh
+++ bikini/theBook.sh
@@ -2,3 +2,25 @@
 
 # Begnining to the journal functions of RJP.
 
+dateFormat=$(date +%F)
+
+
+echo "Welcome to Recovery Journal $user.  Today's date is:   $dateFormat"
+
+echo "1. Read Journal"
+echo "2. New Entry"
+echo "3. List Entries"
+echo "4. Ready Entry"
+
+read option
+
+if $option=1
+then
+echo "Read Journal"
+fi
+
+touch $dateFormat.txt
+
+echo $(date) >> $dateFormat.txt
+
+nano $dateFormat.txt
