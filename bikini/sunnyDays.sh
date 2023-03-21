eb1e1ab3d19c4cef3c68b715285782ab0644b788
kylejamesbyron <markandrewisom@gmail.com>
2023-03-21 13:55:35 -0400

    Making changes to days sober.

--- bikini/sunnyDays.sh
+++ bikini/sunnyDays.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 # A script to determine your sober date.
 
-now=$(date +%s)
-echo $now
+#now=$(date +%s)
+#echo $now
 
-past=$(date +%s --date "1999-12-28")
-echo $past
+#past=$(date +%s --date "1999-12-28")
+#echo $past
 
-difference=$(($now-$past))
-echo $difference
+#difference=$(($now-$past))
+#echo $difference
 
-seconds=$(($difference/(3600*24)))
-echo $seconds
+#seconds=$(($difference/(3600*24)))
+#echo $seconds
 
-echo $(($seconds/364))
+#echo $(($seconds/364))
+
+#example
+#let DIFF=($(date +%s -d 20210131)-$(date +%s -d 20210101))/86400
+#echo $DIFF
+
+
+#new way of doing.
+echo "What year did you become sober?"
+read yearSober
+
+echo "What month did you become sober?"
+
+DIFF=$(date +%y)
+
+echo "$DIFF"
+
