b11cbdec15f46261c5cffd6de399167690664f7f
kylejamesbyron <markandrewisom@gmail.com>
2023-03-22 13:59:12 -0400

    Getting started on database program

--- rjpInstall.sh
+++ rjpInstall.sh
@@ -22,4 +22,28 @@ read soberDate
 
 echo "That is all we need for now.  One moment while we setup the program.  We  will be ready for your first entry shortly."
 
+touch $lastName.$firstName.csv
 
+DATABASE_FILE=$lastName.$firstName.csv
+ 
+function db_clear() {
+  rm -f "$DATABASE_FILE"
+}
+ 
+function db_set() {
+  echo "$1,$2" >> "$DATABASE_FILE"
+}
+ 
+function db_get() {
+  grep "^$1," "$DATABASE_FILE" | sed -e "s/^$1,//" | tail -n 1
+}
+ 
+function db_remove() {
+  db_set $1 ""
+}
+
+# db_clear
+
+db_set lastName $lastName
+
+echo name=$(db_get lastName)
