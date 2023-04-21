f59344b9de3a2e552422992d24f331628311e32b
kylejamesbyron <markandrewisom@gmail.com>
2023-04-21 11:48:11 -0400

    save

--- rjp.py
+++ rjp.py
@@ -5,9 +5,12 @@ from flask import send_file
 from flask import render_template
 from flask import request
 import os
+from flask import session
+import datetime
+
 
 app = Flask(__name__)
-
+app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 # End of opening
 
 # setup sqlite
@@ -29,6 +32,7 @@ def login():
 def logincheck():
 	username = request.form['username']
 	password = request.form['password']
+	session['user']=username
 	import sqlite3
 	connection = sqlite3.connect("rjp.db")
 	cursor = connection.cursor()
@@ -45,14 +49,36 @@ def logincheck():
 def newentry():
 	return render_template('newentry.html')
 
+@app.route('/newentry/submit', methods=['POST'])
+def newentrysubmit():
+	#username = request.form['username']
+	username=session.get('user') 
+	entry = request.form['newentry']
+	entrydate = datetime.datetime.now()
+	connection = sqlite3.connect("rjp.db")
+	cursor = connection.cursor()
+	cursor.execute("INSERT INTO entries (username, entry, entrydate) values (?, ?, ?)", (username, entry, entrydate))
+	connection.commit()
+	return render_template('/home.html')
 
-# end Login
+# end new entry
 
-#Main Page
-@app.route('/')
-def main():
-	return render_template('main.html')
-# main.html
+# Display Journal
+
+@app.route('/displayjournal')
+def displayjournal():
+	username=session.get('user') 
+	connection = sqlite3.connect("rjp.db")
+	cursor = connection.cursor()
+	selection = cursor.execute('SELECT entrydate, entry from entries WHERE username = ?', [username])
+	for row in selection:
+		entryday = (row[0])
+		entry = (row[1])
+	return render_template('displayjournal.html', entryday=entryday, entry=entry)
+
+
+
+
 
 
 
